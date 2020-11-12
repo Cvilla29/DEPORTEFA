161babb1fb6033e070c465afcc3837ed41470c65
fnmg-2001 <pacocar196@gmail.com>
2020-11-12 14:16:29 +0100

    Cerrar login y cambio de los equipos

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -51,6 +52,10 @@ public class MainController{
 		} else {
 			lblStatus.setText("Acceso Denegado");
 		}
+	}
+	
+	public void cerrarLogin (ActionEvent event) {
+		((Node)event.getSource()).getScene().getWindow().hide();
 	}
 	
 	public void noticias(ActionEvent ev) {
